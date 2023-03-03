d3a3d396fbe2bd0543bdbe070e44101aa31c8d24
nasty <nastytrifono03@gmail.com>
2023-03-04 00:20:21 +0300

    capha

--- YP/Page/Avtorizatsia.xaml.cs
+++ YP/Page/Avtorizatsia.xaml.cs
@@ -15,8 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Web;
-
-
+using System.Text.RegularExpressions;
+using System.Windows.Threading;
 
 namespace YP.Page
 {
@@ -28,11 +28,32 @@ namespace YP.Page
         public static string nomer = "89063490993";
         public static string password = "01";
         string text = String.Empty;
+       
+        DispatcherTimer disTimer = new DispatcherTimer();
+        int sec = 10;
+
         public Avtorizatsia()
         {
             InitializeComponent();
+          
+            
         }
-
+        private void dtTicker(object sender, EventArgs e)
+        {
+            if(sec==0)
+            {
+                otpravka.IsEnabled = true;
+                kod.IsEnabled = false;
+                disTimer.Stop();
+                sec = 10;
+               
+            }
+            else
+            {
+                sec=sec-1;
+                time.Text = "Время закончиться через:" + sec;
+            }
+        }
         private void Nomer_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Enter)
@@ -49,20 +70,44 @@ namespace YP.Page
             }
         }
 
+        public void KOD()
+        {
+            otpravka.IsEnabled=false;
+            kod.IsEnabled = true;
+            kod.Focus();
+            Random random = new Random();
+            text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm !#$%&'()*+,-/:;<=>?^_";
+
+            Regex regex = new Regex("(?=(.*?[A-Z]){2})(?=(.*?[a-z]){3})(?=(.*?[0-9]){2})(?=(.*?[ !#$%&'()*+,-/:;<=>?^_]){1}).{8}");
+            while (regex.IsMatch(text) == false)
+            {
+                text = "";
+                for (int i = 0; i < 8; ++i)
+                    text += ALF[random.Next(ALF.Length)];
+            }
+            string caption = "Запомните код";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBoxResult result;
+            result = MessageBox.Show(text, caption, button, icon, MessageBoxResult.Yes);
+            if (result == MessageBoxResult.OK)
+            {
+                time.Visibility = Visibility.Visible;
+                disTimer.Interval = TimeSpan.FromSeconds(1);
+                disTimer.Tick += dtTicker;
+                disTimer.Start();
+            }
+
+        }
         private void Password_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
                 if (password == Password.Text)
                 {
-                    kod.IsEnabled = true;
-                    otpravka.IsEnabled = true;
-                    kod.Focus();
-                    Random random = new Random();
-                    text = String.Empty;
-                    string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm/*-+., ";
-                    for (int i = 0; i < 8; ++i)
-                        text += ALF[random.Next(ALF.Length)];
+                    
+                    KOD();
                    
                 }
                 else
@@ -70,6 +115,14 @@ namespace YP.Page
                     MessageBox.Show("Пароль не правильный.");
                 }
             }
+        }
+
+        private void otpravka_Click(object sender, RoutedEventArgs e)
+        {
+            
+            text = "";
+            KOD();
+
         }
     }
 }
